53d979f0de29141fc13ce9dffe7672f010af0d53
unknown <khandanamir7@gmail.com>
2025-01-27 16:26:39 +0330

    Location model for geocoding api

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,1 +1,13 @@
+class Location {
+  final int id;
+  final String name;
+  final double latitude;
+  final double longitude;
 
+  Location({
+    required this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
+}
