e54033af8022bd348bf86a5e3755809221597fe6
unknown <khandanamir7@gmail.com>
2025-02-02 17:17:40 +0330

    OpenMeteoApiClient group done

--- packages/open_meteo_api/test/open_meteo_api_client_test.dart
+++ packages/open_meteo_api/test/open_meteo_api_client_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:open_meteo_api/open_meteo_api.dart';
@@ -10,7 +12,25 @@ class MockResponse extends Mock implements http.Response {}
 class FakeUri extends Fake implements Uri {}
 
 void main() {
-  group("OpenMeteoApiClient", () {});
+  late http.Client httpClient;
+  late OpenMeteoApiClient apiClient;
+
+  group("OpenMeteoApiClient", () {
+    // Runs before any test runs.
+    setUpAll(() {
+      // registerFallbackValue(FakeUri()) ensures that if
+      // a mock method is called with an argument of type Uri,
+      // but no valid instance is available, a FakeUri() instance
+      // will be used instead.
+      registerFallbackValue(FakeUri());
+    });
+
+    // Runs before each individual test.
+    setUp(() {
+      httpClient = MockHttpClient();
+      apiClient = OpenMeteoApiClient(httpClient: httpClient);
+    });
+  });
   group("constructor", () {});
   group("locationSearch", () {});
   group("getWeather", () {});
