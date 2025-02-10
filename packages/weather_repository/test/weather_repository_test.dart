4109ff23e22cf2857bffb7d13a50df3c02ed4153
unknown <khandanamir7@gmail.com>
2025-02-10 22:11:54 +0330

    Mock classes created for testing the repository

--- packages/weather_repository/test/weather_repository_test.dart
+++ packages/weather_repository/test/weather_repository_test.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:open_meteo_api/open_meteo_api.dart' as open_meteo_api;
 
 import 'package:weather_repository/weather_repository.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
-}
+class MockOpenMeteoApiClient extends Mock
+    implements open_meteo_api.OpenMeteoApiClient {}
+
+class MockLocation extends Mock implements open_meteo_api.Location {}
+
+class MockWeather extends Mock implements open_meteo_api.Weather {}
+
+void main() {}
