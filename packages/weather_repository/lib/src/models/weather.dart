91d6049ee655661550e4de7c6dfe5dcab42359a4
unknown <khandanamir7@gmail.com>
2025-02-10 19:25:18 +0330

    Weather model created for the repository:

--- packages/weather_repository/lib/src/models/weather.dart
+++ packages/weather_repository/lib/src/models/weather.dart
@@ -1,1 +1,41 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'weather.g.dart';
+
+enum WeatherCondition {
+  clear,
+  rainy,
+  cloudy,
+  snowy,
+  unknown,
+}
+
+/// expose a domain-specific weather model.
+/// This model will contain only data relevant to our business cases
+/// in other words it should be completely decoupled from the API client and raw data format.
+@JsonSerializable()
+class Weather extends Equatable {
+  final String location;
+  final double temperature;
+  final WeatherCondition condition;
+
+  const Weather({
+    required this.location,
+    required this.temperature,
+    required this.condition,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        location,
+        temperature,
+        condition,
+      ];
+}
