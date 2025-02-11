3af11f7631885579c8e7b532c6e0c07b732cfaad
unknown <khandanamir7@gmail.com>
2025-02-11 13:49:35 +0330

    Test for locationSearch call

--- packages/weather_repository/test/weather_repository_test.dart
+++ packages/weather_repository/test/weather_repository_test.dart
@@ -27,5 +27,18 @@ void main() {
         expect(WeatherRepository(), isNotNull);
       });
     });
+    group("getWeather", () {
+      const city = 'chicago';
+      const latitude = 41.85003;
+      const longitude = -87.65005;
+      test("calls locatoinSearch with correct city", () async {
+        try {
+          await weatherRepository.getWeather(city);
+        } catch (_) {}
+        verify(
+          () => weatherApiClient.locationSearch(city),
+        ).called(1);
+      });
+    });
   });
 }
