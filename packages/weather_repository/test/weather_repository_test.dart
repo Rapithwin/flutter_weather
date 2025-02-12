d4c9ed9dc2928e084d522bd07a3de0eb14b54f22
unknown <khandanamir7@gmail.com>
2025-02-12 20:55:34 +0330

    Test for success case (rainy)

--- packages/weather_repository/test/weather_repository_test.dart
+++ packages/weather_repository/test/weather_repository_test.dart
@@ -128,6 +128,29 @@ void main() {
               condition: WeatherCondition.cloudy,
             ));
       });
+      test("returns correct response on success rainy)", () async {
+        final location = MockLocation();
+        final weather = MockWeather();
+        when(() => location.name).thenReturn(city);
+        when(() => location.latitude).thenReturn(latitude);
+        when(() => location.longitude).thenReturn(longitude);
+        when(() => weather.temperature).thenReturn(42.42);
+        when(() => weather.weatherCode).thenReturn(99);
+        when(() => weatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+        when(() => weatherApiClient.getWeather(
+              latitude: any(named: "latitude"),
+              longitude: any(named: "longitude"),
+            )).thenAnswer((_) async => weather);
+        final actual = await weatherRepository.getWeather(city);
+        expect(
+            actual,
+            Weather(
+              location: city,
+              temperature: 42.42,
+              condition: WeatherCondition.rainy,
+            ));
+      });
     });
   });
 }
