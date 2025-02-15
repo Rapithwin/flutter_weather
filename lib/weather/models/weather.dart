95845ec572016d1f2c980ed37b022f6243e48b81
unknown <khandanamir7@gmail.com>
2025-02-15 20:16:41 +0330

    Temperature model implemented

--- lib/weather/models/weather.dart
+++ lib/weather/models/weather.dart
@@ -1,6 +1,27 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:weather_repository/weather_repository.dart' hide Weather;
+import 'package:weather_repository/weather_repository.dart'
+    as weather_repository;
+
 enum TemperatureUnits { fahrenheit, celsius }
 
 extension TemperatureUnitsX on TemperatureUnits {
   bool get isFahrenheit => this == TemperatureUnits.fahrenheit;
   bool get isCelsius => this == TemperatureUnits.celsius;
+}
+
+@JsonSerializable()
+class Temperature extends Equatable {
+  final double value;
+
+  const Temperature({required this.value});
+
+  factory Temperature.fromJson(Map<String, dynamic> json) =>
+      _$TemperatureFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TemperatureToJson(this);
+
+  @override
+  List<Object?> get props => [value];
 }
