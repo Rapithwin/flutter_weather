7395258f4a299782b9063e351a88144f6cecdd99
unknown <khandanamir7@gmail.com>
2025-02-16 15:54:59 +0330

    Weather state done

--- lib/weather/cubit/weather_cubit.dart
+++ lib/weather/cubit/weather_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_weather/weather/models/weather.dart';
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 part 'weather_state.dart';
 
--- lib/weather/cubit/weather_state.dart
+++ lib/weather/cubit/weather_state.dart
@@ -9,11 +9,35 @@ extension WeatherStatusX on WeatherStatus {
   bool get isFailure => this == WeatherStatus.failure;
 }
 
-sealed class WeatherState extends Equatable {
-  const WeatherState();
+@JsonSerializable()
+final class WeatherState extends Equatable {
+  WeatherState({
+    this.status = WeatherStatus.initial,
+    this.temperatureUnits = TemperatureUnits.celsius,
+    Weather? weahter,
+  }) : weather = weahter ?? Weather.empty;
+
+  final WeatherStatus status;
+  final Weather weather;
+  final TemperatureUnits temperatureUnits;
+
+  factory WeatherState.fromJson(Map<String, dynamic> json) =>
+      _$WeatherStateFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherStateToJson(this);
+
+  WeatherState copyWith({
+    WeatherStatus? status,
+    Weather? weather,
+    TemperatureUnits? temperatureUnits,
+  }) {
+    return WeatherState(
+      status: status ?? this.status,
+      weahter: weather ?? this.weather,
+      temperatureUnits: temperatureUnits ?? this.temperatureUnits,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [status, weather, temperatureUnits];
 }
-
-final class WeatherInitial extends WeatherState {}
