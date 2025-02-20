55e752827654d623c010955594c1ac9a9edc2919
unknown <khandanamir7@gmail.com>
2025-02-20 20:19:28 +0330

    Added BlocBuilder all the states corresponding to a widget

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_weather/weather/cubit/weather_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// Uses [BlocProvider] in order to provide an instance of the
+/// [WeatherCubit] to the widget tree.
 class WeatherPage extends StatelessWidget {
   const WeatherPage({super.key});
 
@@ -16,6 +20,25 @@ class WeatherPage extends StatelessWidget {
             icon: Icon(Icons.settings),
           )
         ],
+      ),
+      body: Center(
+        child: BlocBuilder<WeatherCubit, WeatherState>(
+          builder: (context, state) {
+            return switch (state.status) {
+              WeatherStatus.initial => const WeatherEmpty(),
+              WeatherStatus.loading => const WeatherLoading(),
+              WeatherStatus.failure => const WeatherError(),
+              WeatherStatus.success => WeatherPopulated(
+                  weather: state.weather,
+                  units,
+                  state.temperatureUnits,
+                  onRefresh: () {
+                    return context.read<WeatherCubit>().refreshWeather();
+                  },
+                ),
+            };
+          },
+        ),
       ),
     );
   }
