d38f25ec2518e1a58ddec77076b64afea1472fef
unknown <khandanamir7@gmail.com>
2025-02-20 20:35:47 +0330

    settings page route defined

---
+++ lib/settings/view/settings_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage._();
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(
+      builder: (_) => const SettingsPage._(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
