a902f993ab1b4d37dcdfc4220183e22a94bf60c5
unknown <khandanamir7@gmail.com>
2025-02-21 21:14:59 +0330

    Added the app to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
+import 'package:bloc_weather/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory(
+            (await getTemporaryDirectory()).path,
+          ),
+  );
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(WeatherApp(weatherRepository: WeatherRepository()));
 }
