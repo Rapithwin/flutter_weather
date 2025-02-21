a5122425130166057f18fc8157cbb1e65f1d1473
unknown <khandanamir7@gmail.com>
2025-02-21 16:11:55 +0330

    Weather widget files created

---
+++ lib/weather/widgets/weather_error.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherError extends StatelessWidget {
+  const WeatherError({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
