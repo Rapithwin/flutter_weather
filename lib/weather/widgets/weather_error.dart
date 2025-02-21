07bde9d108393c55b145b197959ddeb0f22c8039
unknown <khandanamir7@gmail.com>
2025-02-21 18:52:44 +0330

    WeatherError widget implemented

--- lib/weather/widgets/weather_error.dart
+++ lib/weather/widgets/weather_error.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+/// This screen will display if there is an error.
 class WeatherError extends StatelessWidget {
   const WeatherError({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const Text(
+          "🙈",
+          style: TextStyle(fontSize: 64),
+        ),
+        Text(
+          "Something went wrong!",
+          style: theme.textTheme.headlineSmall,
+        )
+      ],
+    );
   }
 }
