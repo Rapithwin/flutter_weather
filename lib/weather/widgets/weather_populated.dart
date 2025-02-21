4e2b0638155e062decb28aef2f09ef45e6c887d9
unknown <khandanamir7@gmail.com>
2025-02-21 19:20:48 +0330

    WeatherBackground widget implemented

--- lib/weather/widgets/weather_populated.dart
+++ lib/weather/widgets/weather_populated.dart
@@ -1,10 +1,52 @@
+import 'package:bloc_weather/weather/weather.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+/// This screen will display after the user has selected a city
+/// and we have recieved the data.
 class WeatherPopulated extends StatelessWidget {
-  const WeatherPopulated({super.key});
+  const WeatherPopulated({
+    super.key,
+    required this.weather,
+    required this.units,
+    required this.onRefresh,
+  });
+
+  final Weather weather;
+  final TemperatureUnits units;
+  final AsyncValueGetter<void> onRefresh;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    return Stack(
+      children: <Widget>[
+        _WeatherBackground(),
+      ],
+    );
+  }
+}
+
+class _WeatherBackground extends StatelessWidget {
+  @override
+  Widget build(context) {
+    final color = Theme.of(context).colorScheme.primaryContainer;
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color,
+              color.brighten(),
+              color.brighten(33),
+              color.brighten(50),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.25, 0.75, 0.90, 1.0],
+          ),
+        ),
+      ),
+    );
   }
 }
