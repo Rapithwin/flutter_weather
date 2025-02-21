a3c94365d742aeb16a34e62cfb48b42d9974ca33
unknown <khandanamir7@gmail.com>
2025-02-21 16:13:57 +0330

    Import barrel

--- lib/weather/view/weather_page.dart
+++ lib/weather/view/weather_page.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:bloc_weather/settings/settings.dart';
 import 'package:bloc_weather/weather/cubit/weather_cubit.dart';
+import 'package:bloc_weather/weather/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
