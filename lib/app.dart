33294f50479ad3bb4c02a6ebd1663d6809cc1196
unknown <khandanamir7@gmail.com>
2025-02-21 21:00:13 +0330

    app.dart created and BlocProvider added

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_weather/weather/cubit/weather_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key, required WeatherRepository weatherRepository})
+      : _weatherRepository = weatherRepository;
+
+  final WeatherRepository _weatherRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => WeatherCubit(_weatherRepository),
+      child: const WeatherAppView(),
+    );
+  }
+}
+
+class WeatherAppView extends StatelessWidget {
+  const WeatherAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
