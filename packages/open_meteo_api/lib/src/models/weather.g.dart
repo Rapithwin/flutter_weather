66c13e055322597c2753d7a123e04c431504d7a8
unknown <khandanamir7@gmail.com>
2025-02-22 18:11:17 +0330

    AutoGenerated toJson and fromJson

--- packages/open_meteo_api/lib/src/models/weather.g.dart
+++ packages/open_meteo_api/lib/src/models/weather.g.dart
@@ -17,8 +17,14 @@ Weather _$WeatherFromJson(Map<String, dynamic> json) => $checkedCreate(
               $checkedConvert('temperature', (v) => (v as num).toDouble()),
           weatherCode:
               $checkedConvert('weathercode', (v) => (v as num).toDouble()),
+          isDay: $checkedConvert('is_day', (v) => (v as num).toInt()),
+          windSpeed: $checkedConvert('windspeed', (v) => (v as num).toDouble()),
         );
         return val;
       },
-      fieldKeyMap: const {'weatherCode': 'weathercode'},
+      fieldKeyMap: const {
+        'weatherCode': 'weathercode',
+        'isDay': 'is_day',
+        'windSpeed': 'windspeed'
+      },
     );
