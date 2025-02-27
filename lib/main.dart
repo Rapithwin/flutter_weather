40483d0e2160acb29fa201aefe7b05d161e0377a
unknown <khandanamir7@gmail.com>
2025-02-27 15:01:35 +0330

    Bloc observer created and added to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:bloc_weather/app.dart';
+import 'package:bloc_weather/weather_bloc_observer.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = const WeatherBlocObserver();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
