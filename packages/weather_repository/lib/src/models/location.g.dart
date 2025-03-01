371d6106f473870ec4e213f52d74cd3abd0ff0e8
unknown <khandanamir7@gmail.com>
2025-03-01 22:39:14 +0330

    New domain level location model

---
+++ packages/weather_repository/lib/src/models/location.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'location.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Location _$LocationFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Location',
+      json,
+      ($checkedConvert) {
+        final val = Location(
+          city: $checkedConvert('city', (v) => v as String),
+          country: $checkedConvert('country', (v) => v as String),
+        );
+        return val;
+      },
+    );
+
+Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'city': instance.city,
+      'country': instance.country,
+    };
