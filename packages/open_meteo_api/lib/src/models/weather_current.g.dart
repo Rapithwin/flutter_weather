c3b0deaf6872d725f1c42f4a0f16f7b218889772
unknown <khandanamir7@gmail.com>
2025-03-26 18:20:44 +0330

    Auto gen fromJson

--- packages/open_meteo_api/lib/src/models/weather_current.g.dart
+++ packages/open_meteo_api/lib/src/models/weather_current.g.dart
@@ -20,12 +20,22 @@ WeatherCurrent _$WeatherCurrentFromJson(Map<String, dynamic> json) =>
               $checkedConvert('weathercode', (v) => (v as num).toDouble()),
           isDay: $checkedConvert('is_day', (v) => (v as num).toInt()),
           windSpeed: $checkedConvert('windspeed', (v) => (v as num).toDouble()),
+          feelsLike: $checkedConvert(
+              'apparent_temperature', (v) => (v as num).toDouble()),
+          humidity: $checkedConvert(
+              'relative_humidity_2m', (v) => (v as num).toInt()),
+          visibility: $checkedConvert('visibility', (v) => (v as num).toInt()),
+          windDirection: $checkedConvert(
+              'wind_direction_10m', (v) => (v as num).toDouble()),
         );
         return val;
       },
       fieldKeyMap: const {
         'weatherCode': 'weathercode',
         'isDay': 'is_day',
-        'windSpeed': 'windspeed'
+        'windSpeed': 'windspeed',
+        'feelsLike': 'apparent_temperature',
+        'humidity': 'relative_humidity_2m',
+        'windDirection': 'wind_direction_10m'
       },
     );
