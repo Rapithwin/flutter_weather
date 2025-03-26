ba3d93ed7efef7165c425a1c3a6a91c7e59099a9
unknown <khandanamir7@gmail.com>
2025-03-26 18:40:34 +0330

    Changed fromJson test for new fields

--- packages/open_meteo_api/test/weather_test.dart
+++ packages/open_meteo_api/test/weather_test.dart
@@ -11,12 +11,20 @@ void main() {
             "weathercode": 63,
             "is_day": 0,
             "windspeed": 4.5,
+            "wind_direction_10m": 196,
+            "apparent_temperature": 12.9,
+            "relative_humidity_2m": 89,
+            "visibility": 13000,
           }),
           isA<WeatherCurrent>()
               .having((w) => w.temperature, "temperature", 15.3)
               .having((w) => w.weatherCode, "weatherCode", 63)
               .having((w) => w.isDay, "isDay", 0)
-              .having((w) => w.windSpeed, "windSpeed", 4.5),
+              .having((w) => w.windSpeed, "windSpeed", 4.5)
+              .having((w) => w.feelsLike, "apparent temperature", 12.9)
+              .having((w) => w.humidity, "relative humidity", 89)
+              .having((w) => w.windDirection, "wind direction", 196)
+              .having((w) => w.visibility, "visibility", 13000),
         );
       });
     });
