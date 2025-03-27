69f69007b69106b3989d106d541aaff8c724aa6e
unknown <khandanamir7@gmail.com>
2025-03-27 19:44:01 +0330

    Auto gen fromJson

--- packages/weather_repository/lib/src/models/weather.g.dart
+++ packages/weather_repository/lib/src/models/weather.g.dart
@@ -23,8 +23,7 @@ Weather _$WeatherFromJson(Map<String, dynamic> json) => $checkedCreate(
               $checkedConvert('feels_like', (v) => (v as num).toDouble()),
           humidity: $checkedConvert('humidity', (v) => (v as num).toInt()),
           visibility: $checkedConvert('visibility', (v) => (v as num).toInt()),
-          windDirection:
-              $checkedConvert('wind_direction', (v) => (v as num).toDouble()),
+          windDirection: $checkedConvert('wind_direction', (v) => v as String),
         );
         return val;
       },
