2912959b1535e6c93675e6586538518945d7b159
unknown <khandanamir7@gmail.com>
2025-03-27 19:09:30 +0330

    Auto gen fromJson

--- packages/weather_repository/lib/src/models/weather.g.dart
+++ packages/weather_repository/lib/src/models/weather.g.dart
@@ -19,10 +19,21 @@ Weather _$WeatherFromJson(Map<String, dynamic> json) => $checkedCreate(
           isDay: $checkedConvert('is_day', (v) => v as bool),
           windSpeed:
               $checkedConvert('wind_speed', (v) => (v as num).toDouble()),
+          feelsLike:
+              $checkedConvert('feels_like', (v) => (v as num).toDouble()),
+          humidity: $checkedConvert('humidity', (v) => (v as num).toInt()),
+          visibility: $checkedConvert('visibility', (v) => (v as num).toInt()),
+          windDirection:
+              $checkedConvert('wind_direction', (v) => (v as num).toDouble()),
         );
         return val;
       },
-      fieldKeyMap: const {'isDay': 'is_day', 'windSpeed': 'wind_speed'},
+      fieldKeyMap: const {
+        'isDay': 'is_day',
+        'windSpeed': 'wind_speed',
+        'feelsLike': 'feels_like',
+        'windDirection': 'wind_direction'
+      },
     );
 
 Map<String, dynamic> _$WeatherToJson(Weather instance) => <String, dynamic>{
@@ -31,6 +42,10 @@ Map<String, dynamic> _$WeatherToJson(Weather instance) => <String, dynamic>{
       'condition': _$WeatherConditionEnumMap[instance.condition]!,
       'is_day': instance.isDay,
       'wind_speed': instance.windSpeed,
+      'feels_like': instance.feelsLike,
+      'humidity': instance.humidity,
+      'visibility': instance.visibility,
+      'wind_direction': instance.windDirection,
     };
 
 const _$WeatherConditionEnumMap = {
