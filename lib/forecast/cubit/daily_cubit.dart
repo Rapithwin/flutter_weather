550608b206dfa41a8fecf5e6a3ee3a5be0df3a9d
unknown <khandanamir7@gmail.com>
2025-05-08 19:28:09 +0330

    Daily forecast states implemented

--- lib/forecast/cubit/daily_cubit.dart
+++ lib/forecast/cubit/daily_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_weather/forecast/models/daily.dart';
+import 'package:bloc_weather/weather/weather.dart';
 import 'package:equatable/equatable.dart';
 
 part 'daily_state.dart';
--- lib/forecast/cubit/daily_state.dart
+++ lib/forecast/cubit/daily_state.dart
@@ -1,10 +1,25 @@
 part of 'daily_cubit.dart';
 
+enum ForecastStatus { initial, loading, success, failure }
+
+extension ForecastStatusX on ForecastStatus {
+  bool get isInitial => this == ForecastStatus.initial;
+  bool get isLoading => this == ForecastStatus.loading;
+  bool get isSuccess => this == ForecastStatus.success;
+  bool get isFailure => this == ForecastStatus.failure;
+}
+
 sealed class DailyState extends Equatable {
-  const DailyState();
+  DailyState({
+    this.status = ForecastStatus.initial,
+    WeatherDaily? daily,
+    this.units = Units.metric,
+  }) : daily = daily ?? WeatherDaily.empty;
+
+  final ForecastStatus status;
+  final WeatherDaily daily;
+  final Units units;
 
   @override
   List<Object> get props => [];
 }
-
-final class DailyInitial extends DailyState {}
