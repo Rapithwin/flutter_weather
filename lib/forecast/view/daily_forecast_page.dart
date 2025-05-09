2232f64842dc21c81ab03ea3a974758749fee358
unknown <khandanamir7@gmail.com>
2025-05-09 19:49:24 +0330

    BlocBuilder added to daily_forecast_page

--- lib/forecast/view/daily_forecast_page.dart
+++ lib/forecast/view/daily_forecast_page.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_weather/forecast/cubit/daily_cubit.dart';
+import 'package:bloc_weather/forecast/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ForecastPage extends StatelessWidget {
   const ForecastPage({super.key});
@@ -7,20 +10,29 @@ class ForecastPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        title: Text(
-          "7-day Forecast",
-          style: theme.textTheme.displaySmall?.copyWith(
-            color: theme.colorScheme.onPrimary,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            "7-day Forecast",
+            style: theme.textTheme.displaySmall?.copyWith(
+              color: theme.colorScheme.onPrimary,
+            ),
           ),
         ),
-      ),
-      // body: ListView.builder(
-      //   itemBuilder: (context, index) {
-      //     return Container();
-      //   },
-      // ),
-    );
+        body: Center(
+          child: BlocBuilder<DailyCubit, DailyState>(
+            builder: (context, state) {
+              return switch (state.status) {
+                ForecastStatus.initial => const ForecastEmpty(),
+                ForecastStatus.loading => const ForecastLoading(),
+                ForecastStatus.failure => const ForecastError(),
+                ForecastStatus.success => ForecastSuccess(
+                    daily: state.daily,
+                    units: state.units,
+                  ),
+              };
+            },
+          ),
+        ));
   }
 }
