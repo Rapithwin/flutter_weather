d60ca295e7eca4f5882e785481540b421be29a23
unknown <khandanamir7@gmail.com>
2025-05-09 19:33:12 +0330

    Added fetchDaily method to cubit

--- lib/forecast/cubit/daily_cubit.dart
+++ lib/forecast/cubit/daily_cubit.dart
@@ -2,9 +2,35 @@ import 'package:bloc/bloc.dart';
 import 'package:bloc_weather/forecast/models/daily.dart';
 import 'package:bloc_weather/weather/weather.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_repository/weather_repository.dart'
+    show WeatherRepository;
 
 part 'daily_state.dart';
 
 class DailyCubit extends Cubit<DailyState> {
-  DailyCubit() : super(DailyInitial());
+  DailyCubit(this._weatherRepository) : super(DailyState());
+
+  final WeatherRepository _weatherRepository;
+
+  Future<void> fetchDaily(
+    double? latitude,
+    double? longitude,
+  ) async {
+    if (latitude == null || longitude == null) return;
+    emit(state.copyWith(status: ForecastStatus.loading));
+
+    try {
+      final weatherDaily = WeatherDaily.fromRepository(
+        await _weatherRepository.getForecastDaily(latitude, longitude),
+      );
+      emit(state.copyWith(
+          status: ForecastStatus.success,
+          units: state.units,
+          daily: weatherDaily));
+    } on Exception {
+      emit(state.copyWith(
+        status: ForecastStatus.failure,
+      ));
+    }
+  }
 }
