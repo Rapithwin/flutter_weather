e35982fa24ff55f200f0f72e659902589c56e9d3
unknown <khandanamir7@gmail.com>
2025-05-13 19:49:55 +0330

    Loading page

--- lib/forecast/widgets/forecast_loading.dart
+++ lib/forecast/widgets/forecast_loading.dart
@@ -5,6 +5,11 @@ class ForecastLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final ThemeData theme = Theme.of(context);
+    return Center(
+      child: CircularProgressIndicator(
+        color: theme.colorScheme.onPrimary,
+      ),
+    );
   }
 }
