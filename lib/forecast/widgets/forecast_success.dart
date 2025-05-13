afd1c2858b85e1bdaaa2fc72eda91f95fe294821
unknown <khandanamir7@gmail.com>
2025-05-13 20:14:37 +0330

    Added ListView and Icon

--- lib/forecast/widgets/forecast_success.dart
+++ lib/forecast/widgets/forecast_success.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_weather/forecast/models/daily.dart';
 import 'package:bloc_weather/weather/models/weather.dart';
+import 'package:bloc_weather/weather/widgets/widgets.dart' show WeatherIcon;
 import 'package:flutter/material.dart';
 
 class ForecastSuccess extends StatelessWidget {
@@ -13,12 +14,34 @@ class ForecastSuccess extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: daily.time.length,
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        return Container();
-      },
+    final size = MediaQuery.sizeOf(context);
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        height: size.height / 2,
+        child: ListView.builder(
+          itemCount: daily.time.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return SizedBox(
+              width: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  WeatherIcon(
+                    condition: daily.condition[index],
+                    isDay: true,
+                    iconSize: 30,
+                  ),
+                  Text("info"),
+                  Text("info"),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
