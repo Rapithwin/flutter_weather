4899342e261286a5d62f5971c17c04bf652f780a
unknown <khandanamir7@gmail.com>
2025-05-13 21:38:58 +0330

    Prototype UI for forecast page

--- lib/forecast/widgets/forecast_success.dart
+++ lib/forecast/widgets/forecast_success.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_weather/forecast/models/daily.dart';
+import 'package:bloc_weather/weather/extensions/extensions.dart';
 import 'package:bloc_weather/weather/models/weather.dart';
 import 'package:bloc_weather/weather/widgets/widgets.dart' show WeatherIcon;
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class ForecastSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
+    final ThemeData theme = Theme.of(context);
+
     return Align(
       alignment: Alignment.topCenter,
       child: Container(
@@ -29,13 +32,55 @@ class ForecastSuccess extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  WeatherIcon(
-                    condition: daily.condition[index],
-                    isDay: true,
-                    iconSize: 30,
+                  Column(
+                    spacing: 15,
+                    children: <Widget>[
+                      Text(daily.time[index]),
+                      WeatherIcon(
+                        condition: daily.condition[index],
+                        isDay: true,
+                        iconSize: 30,
+                      ),
+                      Text(
+                        daily.temperatureMax[index].toString(),
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                  Text("info"),
-                  Text("info"),
+                  Column(
+                    spacing: 15,
+                    children: <Widget>[
+                      Text(
+                        daily.temperatureMin[index].toString(),
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      WeatherIcon(
+                        condition: daily.condition[index],
+                        isDay: false,
+                        iconSize: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            IconData(
+                              daily.windDirection[index].windDirectionToIcon(),
+                              fontFamily: "CustomIcons",
+                            ),
+                            size: 30,
+                            color: theme.colorScheme.onPrimary,
+                          ),
+                          Text(
+                            daily.windSpeed[index].formattedSpeed(units),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
                 ],
               ),
             );
